25e0a656311adc87277836fbda5a532959b20d48
Pd <Aksorn_AI@AksornIDG003.local>
2025-07-03 02:12:06 +0700

    fix-vercel-2

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def process_image():
         print("Exception in /process_image:", traceback.format_exc())
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0', port=5003)
-    app.run()
+    # app.run()
 
